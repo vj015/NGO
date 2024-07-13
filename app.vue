13755de923d9a5d98a0c2c6e5b86e51941a2b8aa
Vipul Jain <vjain2376@gmail.com>
2024-07-13 13:02:20 +0530

    tailwind installed and meta tags added

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="font-mono">{{ "Hello" }}</h1>
   </div>
 </template>
+<script setup lang="ts">
+useSeoMeta({
+  title: "Asha Ki Kiran",
+  ogTitle: "Asha Ki Kiran",
+  description: "NGO which serves humanity and childcare",
+  ogDescription: "NGO which serves humanity and childcare",
+  ogImage: "/logo.jpg",
+});
+</script>
