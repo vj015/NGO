f9c0b5040a2b5c3326ee9b5bc857c13c914966a5
Vipul Jain <vjain2376@gmail.com>
2024-07-18 20:40:31 +0530

    mobile navbar updated

--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -3,12 +3,26 @@
     class="bg-white customh md:hidden w-full text-center font-extrabold border-red-500 font-mono text-4xl flex flex-col items-center justify-around flex-wrap"
   >
     <div>
-      <a href="#" class="block custom-red px-3 pb-6 hover:border-b-4 border-red-500">Home</a>
-      <a href="#" class="block custom-red px-3 py-6 hover:border-b-4 border-red-500">About</a>
-      <a href="#" class="block custom-red px-3 py-6 hover:border-b-4 border-red-500"
+      <a
+        href="#"
+        class="block custom-red px-3 pb-6 hover:underline hover:cursor-pointer"
+        >Home</a
+      >
+      <a
+        href="#"
+        class="block custom-red px-3 py-6 hover:underline hover:cursor-pointer"
+        >About</a
+      >
+      <a
+        href="#"
+        class="block custom-red px-3 py-6 hover:underline hover:cursor-pointer"
         >Fundraising</a
       >
-      <a href="#" class="block custom-red px-3 py-6 hover:border-b-4 border-red-500">Contact</a>
+      <a
+        href="#"
+        class="block custom-red px-3 py-6 hover:underline hover:cursor-pointer"
+        >Contact</a
+      >
     </div>
     <sociallinks class="" />
   </div>
