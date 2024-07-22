ba62722e61314a50eec5109530c1d752f1f273f6
Vipul Jain <vjain2376@gmail.com>
2024-07-23 01:36:17 +0530

    migrated team images to webp

--- pages/team.vue
+++ pages/team.vue
@@ -13,63 +13,63 @@
 <script setup>
 const slides = [
   {
-    url: "/team/Vipul Jain.png",
+    url: "/team/Vipul Jain.webp",
     name: "Vipul Jain",
     title: "Founder & President",
     content:
       "Vipul Jain is the visionary founder of Asha Ki Kiran, committed to transforming society through education and various humanitarian campaigns. He currently operates three free education centers across different locations, providing free education to 200 children, and is deeply dedicated to making a positive impact in the community.",
   },
   {
-    url: "/team/Vaishnavi Saxena.png",
+    url: "/team/Vaishnavi Saxena.webp",
     name: "Vaishnavi Saxena",
     title: "Co-Founder & General Secretary",
     content:
       "Vaishnavi Saxena is the co-founder and general secretary of Asha Ki Kiran handling all backend operations. She is the mastermind behind many campaigns and efficiently manages all our online platforms, ensuring smooth and effective communication and coordination.",
   },
   {
-    url: "/team/Sarthak Sharma.jpg",
+    url: "/team/Sarthak Sharma.webp",
     name: "Sarthak Sharma",
     title: "Co-Founder & Vice President",
     content:
       "Sarthak Sharma is the co-founder and vice president of our NGO. He oversees all operations and is responsible for fundraising to support various social work campaigns, ensuring the successful execution and sustainability of our initiatives.",
   },
   {
-    url: "/team/Vaishnavi Dixit.jpg",
+    url: "/team/Vaishnavi Dixit.webp",
     name: "Vaishnavi Dixit",
     title: "Member & Active Volunteer",
     content:
       "Vaishnavi Dixit is an active social worker who has been with Asha Ki Kiran for the past 2 years. She has participated in numerous activities and campaigns and now leads a project of Asha Ki Kiran Waste2Wonder that focuses on creating art and craft from waste and plastic, promoting sustainability and creativity.",
   },
   {
-    url: "/team/Ridhima.jpg",
+    url: "/team/Ridhima.webp",
     name: "Ridhima Gupta",
     title: "Member & Active Volunteer",
     content:
       "Ridhima Gupta is an active social worker engaged in various social causes across India. She is a valued member of the Asha Ki Kiran family. In collaboration with her NGO, Asha Ki Kiran operates one of its centers located in Panki, working together to make a positive impact in the community.",
   },
   {
-    url: "/team/Ragini Sahu.jpg",
+    url: "/team/Ragini Sahu.webp",
     name: "Ragini Sahu",
     title: "Member & Active Volunteer",
     content:
       "Ragini Sahu, a dedicated graduate currently preparing for government services, An active volunteer at Asha Ki Kiran. She contributes to social causes and shares her knowledge as a teacher at Akk Kakadev Classes, exemplifying her commitment to education and community service.",
   },
   {
-    url: "/team/Khushi kaushik.png",
+    url: "/team/Khushi kaushik.webp",
     name: "Khushi Kaushik",
     title: "Member & Active Volunteer",
     content:
       "Khushi Kaushik is a dedicated 3rd-year student at Armapur PG College. Alongside her studies, she actively volunteers at Asha Ki Kiran, contributing to various social causes. Additionally, she imparts knowledge and teaches at Akk Kakadev Classes, demonstrating her commitment to education and community service.",
   },
   {
-    url: "/team/Tanu Mishra.png",
+    url: "/team/Tanu Mishra.webp",
     name: "Tanu Mishra",
     title: "Member & Active Volunteer",
     content:
       "Tanu Mishra is a dedicated student pursuing her post-graduation. Alongside her academic endeavors, she actively volunteers at Asha Ki Kiran, contributing to various social causes. She also shares her knowledge and passion for teaching at Akk Kakadev Classes, demonstrating her commitment to education and community service.",
   },
   {
-    url: "/team/Priyanshi.png",
+    url: "/team/Priyanshi.webp",
     name: "Priyanshi",
     title: "Member & Active Volunteer",
     content:
